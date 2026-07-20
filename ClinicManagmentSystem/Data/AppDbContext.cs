cdfc1b1a0f712c3b12264533a06e08674615e09c
jamila2006 <cemile.nasirova2006@gmail.com>
2026-07-21 00:16:30 +0400

    Checkpoint 1: configure AppDbContext and initial migration

---
+++ ClinicManagmentSystem/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ClinicManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicManagementSystem.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Appointment> Appointments{ get; set; }
+        public DbSet<Department> Departments{ get; set; }
+        public DbSet<Doctor> Doctors{ get; set; }
+        public DbSet<Patient> Patients{ get; set; }
+    }
+}
