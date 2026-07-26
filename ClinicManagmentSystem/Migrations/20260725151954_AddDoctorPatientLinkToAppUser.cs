0a48bc58e8d3b9e9674cc9ab454bfcf8caac76bd
jamila2006 <cemile.nasirova2006@gmail.com>
2026-07-26 13:41:56 +0400

    Week2 - Checkpoint1: Add migration for AppUser-Doctor/Patient FK columns

---
+++ ClinicManagmentSystem/Migrations/20260725151954_AddDoctorPatientLinkToAppUser.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDoctorPatientLinkToAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DoctorId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PatientId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_DoctorId",
+                table: "AspNetUsers",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_PatientId",
+                table: "AspNetUsers",
+                column: "PatientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Doctors_DoctorId",
+                table: "AspNetUsers",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Patients_PatientId",
+                table: "AspNetUsers",
+                column: "PatientId",
+                principalTable: "Patients",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Doctors_DoctorId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Patients_PatientId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_DoctorId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_PatientId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DoctorId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PatientId",
+                table: "AspNetUsers");
+        }
+    }
+}
