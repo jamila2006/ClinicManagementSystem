b008d8394dfa8b74c78ad0f2c4627a7ae74c3a63
jamila2006 <cemile.nasirova2006@gmail.com>
2026-07-26 13:40:59 +0400

    Week2 - Checkpoint1: Add RoleSeeder for automatic role seeding

---
+++ ClinicManagmentSystem/Data/RoleSeeder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ClinicManagementSystem.Constants;
+
+namespace ClinicManagementSystem.Data
+{
+    public static class RoleSeeder
+    {
+        public static async Task SeedRolesAsync(IServiceProvider services)
+        {
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            var roles = new[] { Roles.Admin, Roles.Doctor, Roles.Patient };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+}
+
