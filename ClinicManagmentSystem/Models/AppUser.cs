18b17d3e3a4c1284c65538ced14fc169cd9eb185
jamila2006 <cemile.nasirova2006@gmail.com>
2026-07-26 13:41:19 +0400

    Week2 - Checkpoint1: Link AppUser to Doctor and Patient entities

--- ClinicManagmentSystem/Models/AppUser.cs
+++ ClinicManagmentSystem/Models/AppUser.cs
@@ -4,6 +4,11 @@ namespace ClinicManagementSystem.Models
 {
     public class AppUser : IdentityUser
     {
+        public int? DoctorId { get; set; }
+        public Doctor? Doctor { get; set; }
 
+        public int? PatientId { get; set; }
+        public Patient? Patient { get; set; }
     }
 }
+
