c1a92f3a387357003b8d12cdc7abc4448b009d9b
jamila2006 <cemile.nasirova2006@gmail.com>
2026-07-26 13:38:21 +0400

    Week2 - Checkpoint1: Add Roles constants (ADMIN, DOCTOR, PATIENT)

---
+++ ClinicManagmentSystem/Constants/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace ClinicManagementSystem.Constants
+{
+    public static class Roles
+    {
+        public const string Admin = "ADMIN";
+        public const string Doctor = "DOCTOR";
+        public const string Patient = "PATIENT";
+    }
+}
