5dde2c74d76cfd92c1cb92b9fc84c3b8cceadfa2
jamila2006 <cemile.nasirova2006@gmail.com>
2026-08-12 15:38:57 +0400

    feat(week4-checkpoint1): apply caching to SpecialtyService GetAll and GetById

--- ClinicManagmentSystem/Services/SpecialtyService.cs
+++ ClinicManagmentSystem/Services/SpecialtyService.cs
@@ -7,17 +7,31 @@ namespace ClinicManagementSystem.Services
     public class SpecialtyService : ISpecialtyService
     {
         private readonly ISpecialtyRepository _repository;
-        public SpecialtyService(ISpecialtyRepository repository) => _repository = repository;
-
+        private readonly ICacheService _cacheService;
+        public SpecialtyService(ISpecialtyRepository repository, ICacheService cacheService)
+        {
+            _repository = repository;
+            _cacheService = cacheService;
+        }
         public async Task<List<SpecialtyDto>> GetAllAsync()
         {
-            var specialties = await _repository.GetAllAsync();
+            var specialties = await _cacheService.GetOrCreateAsync(
+                "specialties:all",
+                async () => await _repository.GetAllAsync()
+            );
             return specialties.Select(ToDto).ToList();
         }
 
+
         public async Task<SpecialtyDto?> GetByIdAsync(int id)
         {
-            var specialty = await _repository.GetByIdAsync(id);
+            var cacheKey = $"specialty:{id}";
+
+            var specialty = await _cacheService.GetOrCreateAsync(
+                cacheKey,
+                async () => await _repository.GetByIdAsync(id)
+            );
+
             return specialty == null ? null : ToDto(specialty);
         }
 
