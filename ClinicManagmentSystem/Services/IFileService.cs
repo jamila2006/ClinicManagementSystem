70a93ebea746384a1dfafe3257e5a7cdad90d9a1
jamila2006 <cemile.nasirova2006@gmail.com>
2026-08-13 18:46:59 +0400

    Week 4 Checkpoint 2-Add file service interface

---
+++ ClinicManagmentSystem/Services/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace ClinicManagementSystem.Services
+{
+    public interface IFileService
+    {
+        Task<string> SaveDoctorPhotoAsync(int doctorId, IFormFile file);
+        Task<(byte[] Content, string ContentType, string FileName)?> GetDoctorPhotoAsync(string fileName);
+        void DeleteDoctorPhotoAsync(string? fileName);
+    }
+}
