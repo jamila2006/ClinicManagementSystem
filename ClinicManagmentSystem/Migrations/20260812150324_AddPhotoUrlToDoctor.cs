77747c590abf91b2314cc55bf89db27973aa7674
jamila2006 <cemile.nasirova2006@gmail.com>
2026-08-13 18:45:44 +0400

    Week 4, Checkpoint 2-Add photo URL to doctor migration

---
+++ ClinicManagmentSystem/Migrations/20260812150324_AddPhotoUrlToDoctor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhotoUrlToDoctor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Doctors");
+        }
+    }
+}
